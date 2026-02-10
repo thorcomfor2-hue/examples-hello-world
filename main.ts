1771680b7d57caa56509fe14c944e9d357c6d058
thorcomfor2-hue <thor.comfor2@gmail.com>
2026-02-10 22:37:19 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,50 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { DOMParser } from "https://esm.sh/linkedom@0.14.16";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+serve(async (req: Request) => {
+  const url = new URL(req.url);
+  const artist = url.searchParams.get("artist");
+
+  const res = await fetch(`https://${artist}.bandcamp.com/music`);
+
+  if (!artist || !res.ok) {
+    return Response.json(
+      {
+        error:
+          "Something is broken here. Did you add an artist search param? For example: https://rss-bandcamp.deno.dev/?artist=plash",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  const html = await res.text();
+  const document = new DOMParser().parseFromString(html, "text/html");
+  const items = Array.from(document.querySelectorAll("#music-grid li")).map(
+    (item) => {
+      const title = item
+        .querySelector(".title")
+        .textContent.replace(/(\r\n|\n|\r)/gm, "")
+        .trim();
+      const url = `https://${artist}.bandcamp.com/${
+        item.querySelector("a").href
+      }`;
+
+      return {
+        title,
+        url,
+        content_html: title,
+      };
+    }
+  );
+
+  return Response.json({
+    version: "https://jsonfeed.org/version/1.1",
+    title: `Bandcamp: ${artist}`,
+    home_page_url: `https://${artist}.bandcamp.com`,
+    feed_url: `https://rss-bandcamp.deno.dev?artist=${artist}`,
+    favicon: "https://s4.bcbits.com/img/favicon/apple-touch-icon.png",
+    items,
+  });
 });
